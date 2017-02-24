9c54edebb4b719bdae5552d119fd84af247f311e
MarlonP <marlon.pavanello@sky.com>
2017-02-24 18:01:42 +0000

    Feed, Post and Like Function

---
+++ EatFlyApp/Post.swift
@@ -0,0 +1,22 @@
+//
+//  Post.swift
+//  EatFlyApp
+//
+//  Created by Marlon Pavanello on 24/02/2017.
+//  Copyright © 2017 Marlon Pavanello. All rights reserved.
+//
+
+import UIKit
+
+class Post: NSObject {
+    
+    var author: String!
+    var likes: Int!
+    var pathToImage: String!
+    var userID: String!
+    var postID: String!
+    
+    var peopleWhoLike: [String] = [String]()
+    
+
+}
